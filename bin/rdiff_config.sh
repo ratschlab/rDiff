1690e24b765a98bdea2580a2aa1905376b4b0c5d
raetsch <raetsch@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2012-02-13 22:48:44 +0000

    missing checkins

--- bin/rdiff_config.sh
+++ bin/rdiff_config.sh
@@ -6,16 +6,14 @@
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
-# Written (W) 2009-2011 Regina Bohnert, Gunnar Raetsch
-# Copyright (C) 2009-2011 Max Planck Society
 #
 
 
 
 export RDIFF_VERSION=0.1
-export RDIFF_PATH=/fml/ag-raetsch/home/bohnert/svn/releases/rDiff/trunk
-export RDIFF_SRC_PATH=/fml/ag-raetsch/home/bohnert/svn/releases/rDiff/trunk/src
-export RDIFF_BIN_PATH=/fml/ag-raetsch/home/bohnert/svn/releases/rDiff/trunk/bin
+export RDIFF_PATH=/fml/ag-raetsch/home/raetsch/svn/releases/rDiff/trunk
+export RDIFF_SRC_PATH=/fml/ag-raetsch/home/raetsch/svn/releases/rDiff/trunk/src
+export RDIFF_BIN_PATH=/fml/ag-raetsch/home/raetsch/svn/releases/rDiff/trunk/bin
 export INTERPRETER=octave
 export MATLAB_BIN_PATH=/fml/ag-raetsch/share/software/matlab/bin/matlab
 export MATLAB_MEX_PATH=/fml/ag-raetsch/share/software/matlab/bin/mex
@@ -23,5 +21,5 @@ export MATLAB_INCLUDE_DIR=/fml/ag-raetsch/share/software/matlab/extern/include
 export OCTAVE_BIN_PATH=/fml/ag-raetsch/share/software/octave-3.0.3/bin/octave
 export OCTAVE_MKOCT=/fml/ag-raetsch/share/software/octave-3.0.3/bin/mkoctfile
 export SAMTOOLS_DIR=/fml/ag-raetsch/share/software/samtools
-export PYTHON_PATH=/usr/bin/python2.5
+export PYTHON_PATH=/usr/bin/python
 export SCIPY_PATH=/fml/ag-raetsch/home/vipin/lib/python2.5/site-packages/
