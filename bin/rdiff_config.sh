2250b01d6ba2f157f0847f64f512d704e6827e21
drewe <drewe@cbio.mskcc.org>
2013-01-16 20:34:56 -0500

    Worked on configuration script

--- bin/rdiff_config.sh
+++ bin/rdiff_config.sh
@@ -10,6 +10,11 @@
 
 
 
+export SAMTOOLS_BIN_DIR=SAMTOOLS_BIN_DIR
+export SAMTOOLS_BIN_DIR=SAMTOOLS_BIN_DIR
+export SAMTOOLS_BIN_DIR=SAMTOOLS_BIN_DIR
+export SAMTOOLS_BIN_DIR=SAMTOOLS_BIN_DIR
+export SAMTOOLS_BIN_DIR=SAMTOOLS_BIN_DIR
 export RDIFF_VERSION=0.1
 export RDIFF_PATH=/Users/drewe/git/software/rDiff
 export RDIFF_SRC_PATH=/Users/drewe/git/software/rDiff/src
@@ -21,5 +26,6 @@ export MATLAB_INCLUDE_DIR=/Applications/MATLAB_R2012b.app/extern/include/
 export OCTAVE_BIN_PATH=/opt/local/bin/octave
 export OCTAVE_MKOCT=/opt/local/bin/mkoctfile
 export SAMTOOLS_DIR=/Users/drewe/Programs/samtools
-export PYTHON_PATH=/opt/local/bin/python2.7
+export SAMTOOLS_BIN_DIR=SAMTOOLS_BIN_DIR
+export PYTHON_PATH=/usr/bin/python
 export SCIPY_PATH=
