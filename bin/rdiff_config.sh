ecf299a3a4a3bd70f8e1c0fdc22e64f9a372badb
Gunnar Raetsch <raetsch@cbio.mskcc.org>
2013-01-30 02:24:54 -0500

    minor polishing

--- bin/rdiff_config.sh
+++ bin/rdiff_config.sh
@@ -9,13 +9,13 @@
 #
 
 export RDIFF_VERSION=0.3
-export RDIFF_PATH=/cbio/grlab/home/raetsch/rdiff
-export RDIFF_SRC_PATH=/cbio/grlab/home/raetsch/rdiff/src
-export RDIFF_BIN_PATH=/cbio/grlab/home/raetsch/rdiff/bin
+export RDIFF_PATH=
+export RDIFF_SRC_PATH=
+export RDIFF_BIN_PATH=
 export RDIFF_INTERPRETER=octave
-export RDIFF_OCTAVE_BIN_PATH=/usr/bin/octave
-export RDIFF_OCTAVE_MKOCT=/usr/bin/mkoctfile
-export RDIFF_SAMTOOLS_BIN_DIR=/cbio/grlab/share/software/samtools/samtools-0.1.18_fPIC/samtools
-export RDIFF_SAMTOOLS_LIB_DIR=/cbio/grlab/share/software/samtools/samtools-0.1.18_fPIC/
-export RDIFF_SAMTOOLS_INCLUDE_DIR=/cbio/grlab/share/software/samtools/samtools-0.1.18_fPIC/
-export RDIFF_PYTHON_PATH=/usr/bin/python
+export RDIFF_OCTAVE_BIN_PATH=
+export RDIFF_OCTAVE_MKOCT=
+export RDIFF_SAMTOOLS_BIN_DIR=
+export RDIFF_SAMTOOLS_LIB_DIR=
+export RDIFF_SAMTOOLS_INCLUDE_DIR=
+export RDIFF_PYTHON_PATH=
